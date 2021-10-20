6d68d728f7028c6b153bc888aedc45688eb3474c
Namit Malasi <namitmalasi@gmail.com>
2021-10-20 17:00:32 +0530

    minor changes

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ const dotenv = require("dotenv");
 const connectDatabase = require("./config/database");
 
 // config
-dotenv.config({ path: "backend/config/config.env" });
+dotenv.config();
 
 // connect to database
 connectDatabase();
