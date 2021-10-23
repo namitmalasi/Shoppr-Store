cd9a0f6272c2ede521707b614f014026ab990213
Namit Malasi <namitmalasi@gmail.com>
2021-10-23 21:51:52 +0530

    added forgot password functionality

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -2,6 +2,7 @@ const ErrorHandler = require("../utils/errorHandler");
 const asyncHandler = require("../middleware/asyncHandler");
 const User = require("../models/userModel");
 const sendToken = require("../utils/jwtToken");
+const sendEmail = require("../utils/sendEmail");
 
 // Register a user
 
@@ -58,4 +59,42 @@ exports.logoutUser = asyncHandler(async (req, res, next) => {
     httpOnly: true,
   });
   res.status(200).json({ success: true, message: "Logged out successfully" });
+});
+
+// Forgot Password
+
+exports.forgotPassword = asyncHandler(async (req, res, next) => {
+  const user = await User.findOne({ email: req.body.email });
+
+  if (!user) {
+    return next(new ErrorHandler("User not found", 404));
+  }
+
+  // get ResetPassword Token
+  const resetToken = user.getResetPasswordToken();
+  await user.save({ validateBeforeSave: false });
+
+  const resetPasswordUrl = `${req.protocol}://${req.get(
+    "host"
+  )}/api/v1/password/reset/${resetToken}`;
+
+  const message = `Your password reset token is :- \n\n ${resetPasswordUrl} \n\n If you have not
+   requested this email then, please ignore it.`;
+
+  try {
+    await sendEmail({
+      email: user.email,
+      subject: `Ecommerce Password Recovery`,
+      message,
+    });
+    res.status(200).json({
+      success: true,
+      message: `Email sent to ${user.email} successfully`,
+    });
+  } catch (error) {
+    user.resetPasswordToken = undefined;
+    user.resetPasswordExpire = undefined;
+    await user.save({ validateBeforeSave: false });
+    return next(new ErrorHandler(error.message, 500));
+  }
 });
--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -4,10 +4,12 @@ const {
   registerUser,
   loginUser,
   logoutUser,
+  forgotPassword,
 } = require("../controllers/userController");
 
 router.route("/register").post(registerUser);
 router.route("/login").post(loginUser);
+router.route("/password/forgot").post(forgotPassword);
 router.route("/logout").get(logoutUser);
 
 module.exports = router;
