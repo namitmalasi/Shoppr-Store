d40a7186e569232600ea6ef356b330bc6281a119
Namit Malasi <namitmalasi@gmail.com>
2021-10-27 17:04:20 +0530

    added get al orders,update orders,delete orders

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -29,3 +29,84 @@ exports.newOrder = asyncHandler(async (req, res, next) => {
 
   res.status(201).json({ success: true, order });
 });
+
+// Get single Order
+exports.getSingleOrder = asyncHandler(async (req, res, next) => {
+  const order = await Order.findById(req.params.id).populate(
+    "user",
+    "name email"
+  );
+
+  if (!order) {
+    return next(
+      new ErrorHandler("Order not found with this Id: ${req.params.id}", 404)
+    );
+  }
+
+  res.status(200).json({ success: true, order });
+});
+
+// Get logged in user's order
+exports.myOrders = asyncHandler(async (req, res, next) => {
+  const orders = await Order.find({ user: req.user._id });
+
+  res.status(200).json({ success: true, orders });
+});
+
+// Get all orders --admin
+exports.getAllOrders = asyncHandler(async (req, res, next) => {
+  const orders = await Order.find();
+
+  let totalAmount = 0;
+  orders.forEach((order) => {
+    totalAmount += order.totalPrice;
+  });
+
+  res.status(200).json({ success: true, totalAmount, orders });
+});
+
+// Update Order status --Admin
+exports.updateOrder = asyncHandler(async (req, res, next) => {
+  const order = await Order.findById(req.params.id);
+
+  if (!order) {
+    return next(new ErrorHandler("Order not found with this Id", 404));
+  }
+
+  if (order.orderStatus === "Delivered") {
+    return next(new ErrorHandler("You have already delivered this order", 400));
+  }
+
+  order.orderItems.forEach(async (order) => {
+    await updateStock(order.product, order.quantity);
+  });
+
+  order.orderStatus = req.body.status;
+
+  if (req.body.status === "Delivered") {
+    order.deliveredAt = Date.now();
+  }
+  await order.save({ validateBeforeSave: false });
+  res.status(200).json({ success: true });
+});
+
+async function updateStock(id, quantity) {
+  const product = await Product.findById(id);
+
+  product.stock -= quantity;
+
+  await product.save({ validateBeforeSave: false });
+}
+
+// delete Order --Admin
+exports.deleteOrder = asyncHandler(async (req, res, next) => {
+  const order = await Order.findById(req.params.id);
+
+  if (!order) {
+    return next(new ErrorHandler("Order not found with this Id", 404));
+  }
+
+  await order.remove();
+
+  res.status(200).json({ success: true });
+});
--- backend/routes/orderRoute.js
+++ backend/routes/orderRoute.js
@@ -1,8 +1,28 @@
 const express = require("express");
 const router = express.Router();
-const { newOrder } = require("../controllers/orderController");
+const {
+  newOrder,
+  getSingleOrder,
+  myOrders,
+  getAllOrders,
+  updateOrder,
+  deleteOrder,
+} = require("../controllers/orderController");
 const { isAuthenticatedUser, authorizeRoles } = require("../middleware/auth");
 
 router.route("/order/new").post(isAuthenticatedUser, newOrder);
+router
+  .route("/order/:id")
+  .get(isAuthenticatedUser, authorizeRoles("admin"), getSingleOrder);
+router.route("/orders/me").get(isAuthenticatedUser, myOrders);
+
+router
+  .route("/admin/order")
+  .get(isAuthenticatedUser, authorizeRoles("admin"), getAllOrders);
+
+router
+  .route("/admin/order/:id")
+  .put(isAuthenticatedUser, authorizeRoles("admin"), updateOrder)
+  .delete(isAuthenticatedUser, authorizeRoles("admin"), deleteOrder);
 
 module.exports = router;
