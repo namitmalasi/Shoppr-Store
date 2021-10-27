1b1b239adc0f3349eb64c3d6f66d31bb625c7445
Namit Malasi <namitmalasi@gmail.com>
2021-10-27 12:40:06 +0530

    added order model and created new order route

---
+++ backend/controllers/orderController.js
@@ -0,0 +1,31 @@
+const Order = require("../models/orderModel");
+const Product = require("../models/productModel");
+const ErrorHandler = require("../utils/errorHandler");
+const asyncHandler = require("../middleware/asyncHandler");
+
+// create new order
+exports.newOrder = asyncHandler(async (req, res, next) => {
+  const {
+    shippingInfo,
+    orderItems,
+    paymentInfo,
+    itemsPrice,
+    taxPrice,
+    shippingPrice,
+    totalPrice,
+  } = req.body;
+
+  const order = await Order.create({
+    shippingInfo,
+    orderItems,
+    paymentInfo,
+    itemsPrice,
+    taxPrice,
+    shippingPrice,
+    totalPrice,
+    paidAt: Date.now(),
+    user: req.user._id,
+  });
+
+  res.status(201).json({ success: true, order });
+});
