d016f85462a251fda6f3fd642cefefcedeb34443
Namit Malasi <namitmalasi@gmail.com>
2021-10-28 12:46:36 +0530

    started the frontend and created the header and footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,3 +1,23 @@
-function App() {}
+import React from "react";
+import Header from "./component/layout/Header/Header.js";
+import Footer from "./component/layout/Footer/Footer.js";
+import { BrowserRouter as Router } from "react-router-dom";
+import WebFont from "webfontloader";
+
+function App() {
+  React.useEffect(() => {
+    WebFont.load({
+      google: {
+        families: ["Roboto", "Droid Sans", "Chilanka"],
+      },
+    });
+  }, []);
+  return (
+    <Router>
+      <Header />
+      <Footer />
+    </Router>
+  );
+}
 
 export default App;
